f4b2186bcdadc6160285a8def56319dc22ff8007
avi3175 <avishahriar@.com>
2023-05-20 01:55:22 +0600

    got the modified

--- index.js
+++ index.js
@@ -35,18 +35,18 @@ async function run() {
     const addToyCollection = client.db('toyWorld').collection('addtoy')
 
 
-    app.get('/toys',async(req,res)=>{
-        const cursor = toysCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get('/toys', async (req, res) => {
+      const cursor = toysCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
     })
 
 
-    app.get('/toys/:id',async(req,res)=>{
-        const id = req.params.id
-        const query = { _id: new ObjectId(id)}
-        const result = await toysCollection.findOne(query)
-        res.send(result)
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.findOne(query)
+      res.send(result)
     })
 
     //*******************************************************************************// 
@@ -54,44 +54,57 @@ async function run() {
     //*******************************************************************************// 
 
     // 1. THIS IS FOR GETTING ALL THE DATA
-    app.get('/addtoy',async(req,res)=>{
+    app.get('/addtoy', async (req, res) => {
       const result = await addToyCollection.find().toArray()
       res.send(result)
     })
 
-  //  2.THIS IS FOR GETTING SPECIFIC DATA
+    //  2.THIS IS FOR GETTING SPECIFIC DATA
 
-  app.get('/addstoy',async(req,res)=>{
-    console.log(req.query.email)
-    let query = {}
-    if(req.query?.email){
-      query={email:req.query.email}
-    }
-    const result = await addToyCollection.find(query).toArray()
-    res.send(result)
-  })
+    app.get('/addstoy', async (req, res) => {
+      console.log(req.query.email)
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await addToyCollection.find(query).toArray()
+      res.send(result)
+    })
 
-    app.post('/addtoy',async(req,res)=>{
+    app.post('/addtoy', async (req, res) => {
       const add = req.body
       console.log(add)
       const result = await addToyCollection.insertOne(add)
       res.send(result)
     })
 
-app.delete('/addstoy/:id',async(req,res)=>{
-  const id = req.params.id
-  const query = {_id:new ObjectId(id)}
-  const result = await addToyCollection.deleteOne(query)
-  res.send(result)
-})
+    app.delete('/addstoy/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await addToyCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
-app.patch('/addstoy/:id',async(req,res)=>{
-  const id = req.params.id
-  const filter = {_id:new ObjectId(id)}
-  const updatedToy = req.body
-  console.log(updatedToy)
-})
+    app.patch('/addstoy/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedToy = req.body
+      console.log(updatedToy)
+
+      const updateDoc = {
+
+        $set: {
+
+          status: updatedToy.status
+
+        },
+
+      };
+      const result = await addToyCollection.updateOne(filter,updateDoc)
+      res.send(result)
+
+    })
 
 
 
@@ -111,10 +124,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-        res.send('CAR IS RUNNING')
+app.get('/', (req, res) => {
+  res.send('CAR IS RUNNING')
 })
 
-app.listen(port,()=>{
-    console.log(`car is running on the port${port}`)
+app.listen(port, () => {
+  console.log(`car is running on the port${port}`)
 })
