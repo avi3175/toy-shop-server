cae833fa9ae0dcd44fecd900dc1136d472f44f64
avi3175 <avishahriar@.com>
2023-05-20 15:55:04 +0600

    taste the server

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ async function run() {
     const addToyCollection = client.db('toyWorld').collection('addtoy')
 
 
-    app.get('/toys', async (req, res) => {
-      const cursor = toysCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    // app.get('/toys', async (req, res) => {
+    //   const cursor = toysCollection.find()
+    //   const result = await cursor.toArray()
+    //   res.send(result)
+    // })
 
 
     app.get('/toys/:id', async (req, res) => {
