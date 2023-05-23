46376d7ba54e1ab1dbde09d9bc156c324cfde607
avi3175 <avishahriar@.com>
2023-05-23 13:28:54 +0600

    server updated

--- index.js
+++ index.js
@@ -29,14 +29,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // client.connect();
 
     const toysCollection = client.db('toyWorld').collection('toys')
     const addToyCollection = client.db('toyWorld').collection('addtoy')
 
 
     app.get('/toys', async (req, res) => {
-      const cursor = toysCollection.find()
+      const query = {}
+      const options = {
+        sort:{"price":-1}
+      }
+      const cursor = toysCollection.find(query,options)
       const result = await cursor.toArray()
       res.send(result)
     })
@@ -67,7 +71,10 @@ async function run() {
       if (req.query?.email) {
         query = { email: req.query.email }
       }
-      const result = await addToyCollection.find(query).toArray()
+      const options = {
+        sort:{"price":-1}
+      }
+      const result = await addToyCollection.find(query,options).toArray()
       res.send(result)
     })
 
@@ -109,7 +116,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
